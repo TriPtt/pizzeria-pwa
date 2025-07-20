8c70cda0589b703cdde16dd14c09cb15f265a26e
Tristan Petit <tristan.petit01@gmail.com>
2025-07-20 19:52:00 +0200

    ajout de l'inscription

--- back/src/app.js
+++ back/src/app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
-
 const app = express();
+const authRoutes = require("./routes/auth");
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+// Routes
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur http://localhost:${PORT}`);
+});
