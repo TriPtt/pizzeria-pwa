24499c7f98a74e774003b362520d8eceecd5b5cb
Tristan <tristan.petit01@gmail.com>
2025-07-24 00:06:18 +0200

    fix import animation.css

--- front/src/main.js
+++ front/src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import 'remixicon/fonts/remixicon.css'
-import './styles/animations.css'
+import './styles/animation.css'
 
 const app = createApp(App)
 const pinia = createPinia()
